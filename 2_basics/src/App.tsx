c8e5e004978adb0008e213521ad629194b5ad471
tanishkahagawane <tanishkahagawane@gmail.com>
2024-10-03 23:19:06 +0530

    Baiscs

---
+++ 2_basics/src/App.tsx
@@ -0,0 +1,11 @@
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div>
+    <Todos items={["learn React","Learn Typescipt"]}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_basics/src/components/Todos.tsx
@@ -0,0 +1,10 @@
+
+const Todos:React.FC<{items:string[]}> = (props)=>{
+    return <ul>
+        {props.items.map((item)=>(
+            <li key={item}>{item}</li>
+        ))}
+    </ul>
+}
+
+export default Todos;
